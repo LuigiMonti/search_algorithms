75ee85b01e32b200c5abbb33b773222b809da894
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-04-29 10:40:52 -0600

    Binary search unit test added

---
+++ UnitTest_Binary.py
@@ -0,0 +1,17 @@
+import pytest
+from searching import binary_search
+
+def test_encontrado_inicio():
+    assert binary_search([1, 2, 3, 4, 5], 1) == 0
+
+def test_encontrado_final():
+    assert binary_search([1, 2, 3, 4, 5], 5) == 4
+
+def test_encontrado_medio():
+    assert binary_search([10, 20, 30, 40, 50], 30) == 2
+
+def test_no_encontrado():
+    assert binary_search([1, 2, 3], 99) == -1
+
+def test_lista_vacia():
+    assert binary_search([], 5) == -1
